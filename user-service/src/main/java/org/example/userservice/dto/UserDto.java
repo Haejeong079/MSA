b62aead79c13819c5a3d18249c9936499d22e8a1
Haejeong079 <goesed7582@naver.com>
2025-04-12 02:08:44 +0900

    :rocket: [Feature] : user-service, dto

--- user-service/src/main/java/org/example/userservice/dto/UserDto.java
+++ user-service/src/main/java/org/example/userservice/dto/UserDto.java
@@ -1,2 +1,21 @@
-package org.example.userservice.dto;public class UserDto {
+package org.example.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private String email;
+    private String name;
+    private String passowrd;
+    private String userId;
+    private Date createdAt;
+
+    private String encryptedPwd;
 }
